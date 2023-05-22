c6aee83eff8638bfc1a7f5b280719bb681dafe05
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2023-05-22 12:13:01 +0200

    calculator functioning for all found rates

--- currencyCalculator.Business/Calculator.cs
+++ currencyCalculator.Business/Calculator.cs
@@ -8,10 +8,26 @@ public class Calculator
     public Calculator() => _rates = new RatesHandler();
     public double calculateCurrency(string fromCurrency, string toCurrency, double amount)
     {
-        var rates = _rates.ReadCurrencies();
-        var foundRate = rates.Where(rate => rate.ToCurrency.Equals(toCurrency)).Select(rate => rate.Rate).FirstOrDefault();
-        var finalRate = foundRate * amount;
-        return finalRate;
+        if (fromCurrency is "" || toCurrency is "") throw new InvalidDataException();
+
+        var allRates = _rates.ReadCurrencies();
+
+        var rateFromEUR = allRates
+                            .Where(rate => rate.ToCurrency.Equals(toCurrency)
+                                            && rate.FromCurrency.Equals("EUR"))
+                            .Select(rate => rate.Rate)
+                            .FirstOrDefault();
+
+        if (rateFromEUR is 0.0) throw new ArgumentNullException();
+
+        var calculateRate = allRates
+                            .Where(currency => !fromCurrency.Equals("EUR")
+                                                    && currency.ToCurrency.Equals(fromCurrency))
+                            .Select(currency => Math.Abs((currency.Rate - rateFromEUR + 1) * amount))
+                            .FirstOrDefault();
+
+        if (calculateRate is 0.0) return rateFromEUR * amount;
+        return calculateRate;
     }
 
 }
--- currencyCalculator.Tests/CurrencyUnitTests.cs
+++ currencyCalculator.Tests/CurrencyUnitTests.cs
@@ -14,4 +14,71 @@ public class CurrencyUnitTests
         //assert
         calculator.Should().Be(11.7010);
     }  
+
+    [Fact]
+    public void calculator_will_throw_if_no_values_are_given()
+    {
+        //arrange
+        var calculatorClass = new Calculator();
+
+        //act
+        var calculator = () => calculatorClass.calculateCurrency("", "", 1.0);
+
+        //assert
+        calculator.Should().Throw<InvalidDataException>();
+    }  
+
+    [Fact]
+    public void calculator_will_throw_if_currency_code_is_not_found()
+    {
+        //arrange
+        var calculatorClass = new Calculator();
+
+        //act
+        var calculator = () => calculatorClass.calculateCurrency("Fiction", "Fictionary", 1.0);
+
+        //assert
+        calculator.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void calculator_calculates_for_differrent_currencies()
+    {
+        //arrange
+        var calculatorClass = new Calculator();
+
+        //act
+        var calculator = calculatorClass.calculateCurrency("EUR", "USD", 50);
+
+        //assert
+        calculator.Should().Be(54.04);
+    }
+
+    [Fact]
+    public void calculator_calculates_for_double_input_values()
+    {
+        //arrange
+        var calculatorClass = new Calculator();
+
+        //act
+        var calculator = calculatorClass.calculateCurrency("EUR", "CHF", 84.9);
+
+        //assert
+        calculator.Should().Be(82.67562000000001);
+    }
+
+    [Fact]
+    public void calculator_calculates_from_other_currencies_than_EUR()
+    {
+        //arrange
+        var calculatorClass = new Calculator();
+
+        //act
+        var calculator = calculatorClass.calculateCurrency("USD", "CHF", 20);
+        var secondCalculator = calculatorClass.calculateCurrency("PLN", "ISK", 20);
+
+        //assert
+        calculator.Should().Be(22.14);
+        secondCalculator.Should().Be(2915.59); 
+    }
 }
