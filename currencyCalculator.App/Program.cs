95028c7aaabc128ab580830e21713084c3f4de15
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2023-05-22 14:13:39 +0200

    Console updated with date option

--- currencyCalculator.App/Program.cs
+++ currencyCalculator.App/Program.cs
@@ -7,15 +7,21 @@ class Program
     static void Main(string[] args)
     {
         var ratesCalculator = new Calculator();
-        Console.WriteLine("Currency Rate Calculator \n Submit in format FromCurrency,ToCurrency,Amount (Example: EUR,USD,10) \n Please communicate currencies in currency codes");
+        Console.WriteLine("Currency Rate Calculator \n Submit in format FromCurrency,ToCurrency,Amount,Date | Date is optional. (Example: EUR,USD,10) \n Please communicate currencies in currency codes");
         var input = Console.ReadLine()?.Split(",");
         
         if (input is null || input[0] is null || input[1] is null || input[2] is null)
         {
             Console.WriteLine("Invalid input. Please state all three values: FromCurrency,ToCurrency,Amount");
         }
-        var currentRate = ratesCalculator.calculateCurrency(input![0],input[1],Convert.ToDouble(input[2]));
 
-        Console.WriteLine($"{input[2]} {input[0]} is {currentRate} {input[1]}");
+        if (input is not null && input.Length is 3)
+        {
+            var currentRate = ratesCalculator.calculateCurrency(input![0], input[1], Convert.ToDouble(input[2]));
+            Console.WriteLine($"{input[2]} {input[0]} is {currentRate} {input[1]}");
+        }
+
+        var rateByDate = ratesCalculator.calculateCurrency(input![0],input[1],Convert.ToDouble(input[2]), input[3]);
+        Console.WriteLine($"{input[2]} {input[0]} is {rateByDate} {input[1]}");
     }
 }
