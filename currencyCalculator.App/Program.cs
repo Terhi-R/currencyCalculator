fa5e4b35ac822a2d13d71eae3395c4489e8349cf
Terhi-R <112477395+Terhi-R@users.noreply.github.com>
2023-05-22 12:37:14 +0200

    currency console calculator

--- currencyCalculator.App/Program.cs
+++ currencyCalculator.App/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace currencyCalculator.App;
+﻿using currencyCalculator.Business;
+
+namespace currencyCalculator.App;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var ratesCalculator = new Calculator();
+        Console.WriteLine("Currency Rate Calculator \n Submit in format FromCurrency,ToCurrency,Amount (Example: EUR,USD,10) \n Please communicate currencies in currency codes");
+        var input = Console.ReadLine()?.Split(",");
+        
+        if (input is null || input[0] is null || input[1] is null || input[2] is null)
+        {
+            Console.WriteLine("Invalid input. Please state all three values: FromCurrency,ToCurrency,Amount");
+        }
+        var currentRate = ratesCalculator.calculateCurrency(input![0],input[1],Convert.ToDouble(input[2]));
+
+        Console.WriteLine($"{input[2]} {input[0]} is {currentRate} {input[1]}");
     }
 }
